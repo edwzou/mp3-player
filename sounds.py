5838cfc5229cf8174e85523f3d91fd727aee5956
Edward Zou <edwardzou@iMac2022.local>
2024-06-16 13:01:06 -0600

    Added delete song option

--- sounds.py
+++ sounds.py
@@ -34,6 +34,29 @@ def add_many_songs():  # Add many songs into the playlist
         playlist.insert(END, song)
 
 
+def delete_song():  # Delete the currently selected song in the playlist
+    playlist.delete(ANCHOR)
+    pygame.mixer.music.stop()
+
+
+def delete_all_songs():  # Delete all songs in the playlist
+    playlist.delete(0, END)
+    pygame.mixer.music.stop()
+
+
+def backward():
+    curr_song = playlist.curselection()  # Get the current song tuple number
+    if curr_song:
+        prev_song = (curr_song[0] - 1) % playlist.size()  # Minus one to the current song number, wrap around
+        song = playlist.get(prev_song)  # Grab song title from playlist
+        song_path = os.path.join(base_path, f'{song}.mp3')
+        pygame.mixer.music.load(song_path)
+        pygame.mixer.music.play(loops=0)
+        playlist.selection_clear(0, END)  # Clear active bar in playlist listbox
+        playlist.activate(prev_song)  # Activate new song bar
+        playlist.selection_set(prev_song, last=None)  # Set active bar to prev song
+
+
 def play():  # Play the selected song
     song = playlist.get(ACTIVE)
     song_path = os.path.join(base_path, f'{song}.mp3')
@@ -56,10 +79,23 @@ def stop():  # Stop the song that's currently playing
     playlist.selection_clear(ACTIVE)
 
 
+def forward():
+    curr_song = playlist.curselection()  # Get the current song tuple number
+    if curr_song:
+        next_song = (curr_song[0] + 1) % playlist.size()  # Add one to the current song number, wrap around
+        song = playlist.get(next_song)  # Grab song title from playlist
+        song_path = os.path.join(base_path, f'{song}.mp3')
+        pygame.mixer.music.load(song_path)
+        pygame.mixer.music.play(loops=0)
+        playlist.selection_clear(0, END)  # Clear active bar in playlist listbox
+        playlist.activate(next_song)  # Activate new song bar
+        playlist.selection_set(next_song, last=None)  # Set active bar to next song
+
+
 playlist = Listbox(root, bg="black", fg="white", width=60, selectbackground="Grey")  # Create song box
 playlist.pack(pady=20, padx=20)
 
-skip_to_start_btn_img = PhotoImage(file="buttons/skip-to-start-btn.png")
+backward_btn_img = PhotoImage(file="buttons/backward-btn.png")
 play_btn_img = PhotoImage(file="buttons/play-btn.png")
 pause_btn_img = PhotoImage(file="buttons/pause-btn.png")
 stop_btn_img = PhotoImage(file="buttons/stop-btn.png")
@@ -68,11 +104,11 @@ forward_btn_img = PhotoImage(file="buttons/forward-btn.png")
 control_frame = Frame(root)  # Create player control frame
 control_frame.pack()
 
-skip_to_start_button = Button(control_frame, image=skip_to_start_btn_img, borderwidth=0)
+skip_to_start_button = Button(control_frame, image=backward_btn_img, borderwidth=0, command=backward)
 play_button = Button(control_frame, image=play_btn_img, borderwidth=0, command=play)
 pause_button = Button(control_frame, image=pause_btn_img, borderwidth=0, command=pause)
 stop_button = Button(control_frame, image=stop_btn_img, borderwidth=0, command=stop)
-forward_button = Button(control_frame, image=forward_btn_img, borderwidth=0)
+forward_button = Button(control_frame, image=forward_btn_img, borderwidth=0, command=forward)
 
 skip_to_start_button.grid(row=0, column=0, padx=5)
 play_button.grid(row=0, column=1, padx=5)
@@ -83,9 +119,14 @@ forward_button.grid(row=0, column=4, padx=5)
 my_menu = Menu(root)
 root.config(menu=my_menu)
 
-add_song_menu = Menu(my_menu)  # Create menu
+add_song_menu = Menu(my_menu)  # Create add song menu
 my_menu.add_cascade(label="Add Songs", menu=add_song_menu)
-add_song_menu.add_command(label="Add One Song to Playlist", command=add_song)  # Add one song at a time to the playlist
+add_song_menu.add_command(label="Add a Song to Playlist", command=add_song)  # Add a song at a time to the playlist
 add_song_menu.add_command(label="Add Many Songs to Playlist", command=add_many_songs)  # Add many songs to the playlist
+
+delete_song_menu = Menu(my_menu)  # Create delete song menu
+my_menu.add_cascade(label="Delete Songs", menu=delete_song_menu)
+delete_song_menu.add_command(label="Delete a Song from Playlist", command=delete_song)  # Delete a song
+delete_song_menu.add_command(label="Delete All Songs from Playlist", command=delete_all_songs)  # Delete all songs
 
 root.mainloop()
